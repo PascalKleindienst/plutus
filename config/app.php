1a94b84800e26943a7d49a558a5a5d73971dd192
pascal <hello@pascalkleindienst.de>
2017-08-10 14:15:50 +0200

    added debugbar

--- config/app.php
+++ config/app.php
@@ -167,6 +167,7 @@ return [
          * Package Service Providers...
          */
          Prettus\Repository\Providers\RepositoryServiceProvider::class,
+         Barryvdh\Debugbar\ServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
     ],
 
 ];
